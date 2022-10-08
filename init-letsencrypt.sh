5514bf98702c06e24d9acf8550105bdfc6eae96f
Thiago Magalhaes <thiago.magalhaes@gmail.com>
2022-10-08 04:01:21 -0300

    Change domain name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(feature-hub.electroneek.com)
+domains=(zabbix.prod.electroneek.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="t.magalhaes@electroneek.com"
